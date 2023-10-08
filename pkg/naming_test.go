ea2e03d9a96f9cd20c847a49c6503acc7af79b74
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2023-10-08 16:50:10 +0530

    Add tests for naming funcs

---
+++ pkg/naming_test.go
@@ -0,0 +1,17 @@
+package pkg
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestToSdkName(t *testing.T) {
+	assert.Equal(t, "stringProp", ToSdkName(ToSdkName("string_prop")))
+}
+
+func TestStartsWithNumber(t *testing.T) {
+	assert.True(t, startsWithNumber("1_var"))
+	assert.True(t, startsWithNumber("1var"))
+	assert.False(t, startsWithNumber("var"))
+}
