768b32419d6e320b3726e17691f36429ae68a457
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2024-04-13 11:12:57 -0700

    Update test

--- pkg/bug_131_test.go
+++ pkg/bug_131_test.go
@@ -23,9 +23,21 @@ func TestBug131(t *testing.T) {
 	assert.Nil(t, err)
 	assert.NotNil(t, providerMetadata)
 
-	funcSpec, ok := testPulumiPkg.Functions[packageName+":actions/v2:listActions"]
-	assert.True(t, ok, "Expected to find function listActions in the Pulumi package spec")
+	t.Run("Renamed", func(t *testing.T) {
+		funcSpec, ok := testPulumiPkg.Functions[packageName+":actions/v2:listActions"]
+		assert.True(t, ok, "Expected to find function listActions in the Pulumi package spec")
 
-	assert.NotEmpty(t, funcSpec.Outputs.Properties["items"].Ref)
-	assert.Equal(t, funcSpec.Outputs.Properties["items"].Ref, "#/types/fake-package:actions/v2:ListActionsItems")
+		assert.NotEmpty(t, funcSpec.Outputs.Properties["items"].Ref)
+		assert.Equal(t, funcSpec.Outputs.Properties["items"].Ref, "#/types/fake-package:actions/v2:ListActionsItems")
+	})
+
+	// Test that a response type without an allOf definition in its
+	// response type is named with the suffix Properties.
+	t.Run("PropertiesResponseType", func(t *testing.T) {
+		funcSpec, ok := testPulumiPkg.Functions[packageName+":actions2/v2:listActions2"]
+		assert.True(t, ok, "Expected to find function listActions2 in the Pulumi package spec")
+
+		assert.NotEmpty(t, funcSpec.Outputs.Properties["items"].Ref)
+		assert.Equal(t, funcSpec.Outputs.Properties["items"].Ref, "#/types/fake-package:actions2/v2:ListActions2Properties")
+	})
 }
