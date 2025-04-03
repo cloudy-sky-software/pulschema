b17b77aafab28872f8b1a46d44c0f3497cf6e39a
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2025-04-02 22:07:21 -0700

    Remove duplicate resource collection for PUT endpoint paths (#252)
    * Consider PUT endpoint paths for creates if the parent path POST is nil in addition to current path
    
    * Fix PUT endpoint path resource collection
    
    * Reset go.mod

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/cloudy-sky-software/pulschema
 
-go 1.22.5
+go 1.23.1
 
 toolchain go1.24.2
 
--- pkg/put_create_resource_with_path_param_test.go
+++ pkg/put_create_resource_with_path_param_test.go
@@ -21,20 +21,43 @@ func TestPutCreateResourceWithPathParam(t *testing.T) {
 		"": "Provider",
 	}
 
-	_, _, err := openAPICtx.GatherResourcesFromAPI(csharpNamespaces)
+	metadata, _, err := openAPICtx.GatherResourcesFromAPI(csharpNamespaces)
 	assert.Nil(t, err)
 
-	subResource, ok := testPulumiPkg.Resources["fake-package:fakeresource/v2:SubResource"]
-	assert.Truef(t, ok, "Expected to find a resource called SubResource: %v", testPulumiPkg.Resources)
+	t.Run("FakeResource", func(t *testing.T) {
+		fakeResource, ok := testPulumiPkg.Resources["fake-package:fakeresource/v2:FakeResource"]
+		assert.Truef(t, ok, "Expected to find a resource called FakeResource: %v", testPulumiPkg.Resources)
 
-	// Ensure that the input properties for the resource contains
-	// the expected id property.
-	assert.Contains(t, subResource.InputProperties, "someId")
+		crudMap, ok := metadata.ResourceCRUDMap["fake-package:fakeresource/v2:FakeResource"]
+		assert.Truef(t, ok, "Expected to find a CRUD map entry for FakeResource")
+		// According the test API spec for FakeResource, it can be created
+		// using a PUT endpoint and there is no POST endpoint for it.
+		// So let's verify that in the metadata as well.
+		assert.NotNil(t, crudMap.P, "PUT endpoint for FakeResource must be non-nil")
+		assert.Equal(t, *crudMap.P, *crudMap.C, "POST endpoint for FakeResource must be the same as PUT endpoint")
 
-	// Ensure that the "get" func also contains the id
-	// as an input properties.
-	getFunc, ok := testPulumiPkg.Functions["fake-package:fakeresource/v2:listSubResources"]
-	assert.Truef(t, ok, "Expected to find a list func listSubResources: %v", testPulumiPkg.Functions)
-	assert.NotNil(t, getFunc.Inputs)
-	assert.Contains(t, getFunc.Inputs.Properties, "someId")
+		// Ensure that the input properties for the resource contains
+		// the expected id property.
+		assert.Contains(t, fakeResource.InputProperties, "someId")
+
+		// Ensure that the "get" func also contains the id
+		// as an input properties.
+		getFunc, ok := testPulumiPkg.Functions["fake-package:fakeresource/v2:listFakeResources"]
+		assert.Truef(t, ok, "Expected to find a list func listFakeResources: %v", testPulumiPkg.Functions)
+		assert.NotNil(t, getFunc.Inputs)
+		assert.Contains(t, getFunc.Inputs.Properties, "someId")
+	})
+
+	t.Run("DifferentResource", func(t *testing.T) {
+		_, ok := testPulumiPkg.Resources["fake-package:differentresource/v2:DifferentResource"]
+		assert.Truef(t, ok, "Expected to find a resource called DifferentResource: %v", testPulumiPkg.Resources)
+
+		crudMap, ok := metadata.ResourceCRUDMap["fake-package:differentresource/v2:DifferentResource"]
+		assert.Truef(t, ok, "Expected to find a CRUD map entry for DifferentResource")
+		// According the test API spec for DifferentResource, it can be created
+		// using the POST endpoint and updated via a PUT endpoint.
+		assert.NotNil(t, crudMap.P, "PUT endpoint for DifferentResource must be non-nil")
+		// The PUT and POST endpoints must be different here.
+		assert.NotEqual(t, *crudMap.P, *crudMap.C, "POST endpoint for DifferentResource must NOT be the same as PUT endpoint")
+	})
 }
